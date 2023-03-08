a9682770349cce1efbe0bdaa034399d2fad80c7b
Piroska Werner <piroska.werner@gmail.com>
2023-03-08 12:09:19 -0600

    feat: Add title based on variable to home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { PricingComponent } from './components/pricing/pricing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomePageComponent,
+    PricingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/home-page/home-page.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.sass']
+})
+export class HomePageComponent implements OnInit {
+  
+  title = 'Angular Home Page'
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
